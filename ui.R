048813fa5697bf2d30e1d3e2847dac2d5a7451f3
RebekahLMiller <RebekahLMiller@gmail.com>
2020-01-28 17:56:40 -0500

    Changed tabs to 4 spaces instead of 2

--- ui.R
+++ ui.R
@@ -1,12 +1,4 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+# The following libraries used here are loaded in app.R
 # library(shiny)
 # library(shinythemes)
 # library(shinyjs)
@@ -14,54 +6,84 @@
 # library(markdown)
 # library(RMySQL)
 
-
-# Define UI for application that will do something at some point
-
+# ???
 navbarPageWithInputs <- function(..., inputs) {
-  navbar <- navbarPage(...)
-  form <- tags$form(class = "navbar-form", inputs)
-  navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(
-    navbar[[3]][[1]]$children[[1]], form)
-  navbar
+    navbar <- navbarPage(...)
+    form <- tags$form(class = "navbar-form", inputs)
+    navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(navbar[[3]][[1]]$children[[1]], form)
+    navbar
 }
 
+# Define UI
 ui <- fluidPage(
-  useShinyalert(),
-  theme=shinytheme("sandstone"),
-  tags$h1("SigRepo"),
-  navbarPageWithInputs(
-  "SigRepo",
-  tabPanel("Upload",
-           sidebarLayout(
-             sidebarPanel(
-               fileInput(inputId="rds_file", label="Signature File", multiple = FALSE, accept = NULL,
-                         width = NULL, buttonLabel = "Browse...",
-                         placeholder = "No file selected"),
-               HTML(
-                 paste("Would be good to link to an example rds file here",
-                          "(Maybe for all levels)"), sep="<br/></br>"),
-               textInput(inputId="signature_name",label="Signature Name",placeholder=NULL),
-               textInput(inputId="group_id",label="Collection Name",placeholder=NULL),
-               selectizeInput(inputId="species_id",label="Species",choices=NULL),
-               textInput(inputId="platform_id",label="Platform",placeholder=NULL),
-               textInput(inputId="cell_line",label="Cell Line(ATCC)",placeholder=NULL),
-               selectizeInput(inputId="keywords",label="Keywords(Optional)",multiple=TRUE,choices=c("a","b","c")),
-               actionButton("add_signature","Add Signature")
-             ),
-             
-             # Show a plot of the generated distribution
-             mainPanel(
-               "could put something here"
-             )
-           )),
-  tabPanel("Summary",
-           tableOutput("featurename_table")
-           ), 
-  inputs=textInput(
-    inputId="search",
-    label="",
-    placeholder="REEEEE",
-    width="100px"
-  )
-))
-
+    useShinyalert(),
+    theme = shinytheme("sandstone"),
+    tags$h1("SigRepo"),
+    navbarPageWithInputs(
+        "SigRepo",
+        tabPanel("Upload",
+            sidebarLayout(
+                sidebarPanel(
+                    fileInput(
+                        inputId = "rds_file",
+                        label = "Signature File",
+                        multiple = FALSE,
+                        accept = NULL,
+                        width = NULL,
+                        buttonLabel = "Browse...",
+                        placeholder = "No file selected"
+                    ),
+                    HTML(
+                        paste(
+                            "Would be good to link to an example rds file here",
+                            "(Maybe for all levels)"
+                        ),
+                        sep = "<br/></br>"
+                    ),
+                    textInput(
+                        inputId = "signature_name",
+                        label = "Signature Name",
+                        placeholder = NULL
+                    ),
+                    textInput(
+                        inputId = "group_id",
+                        label = "Collection Name",
+                        placeholder = NULL
+                    ),
+                    selectizeInput(
+                        inputId = "species_id",
+                        label = "Species",
+                        choices = NULL
+                    ),
+                    textInput(
+                        inputId = "platform_id",
+                        label = "Platform",
+                        placeholder = NULL
+                    ),
+                    textInput(
+                        inputId = "cell_line",
+                        label = "Cell Line(ATCC)",
+                        placeholder = NULL
+                    ),
+                    selectizeInput(
+                        inputId = "keywords",
+                        label = "Keywords(Optional)",
+                        multiple = TRUE,
+                        choices = c("a", "b", "c")
+                    ),
+                    actionButton("add_signature", "Add Signature")
+                ),
+                
+                # Show a plot of the generated distribution
+                mainPanel("could put something here")
+            )),
+        tabPanel("Summary",
+            tableOutput("featurename_table")),
+        inputs = textInput(
+            inputId = "search",
+            label = "",
+            placeholder = "REEEEE",
+            width = "100px"
+        )
+    )
+)
