e3ebba59815d5c5f47d942fa9b20d853fc268387
RebekahLMiller <RebekahLMiller@gmail.com>
2020-02-03 16:15:05 -0500

    Separated upload from main UI file

--- ui.R
+++ ui.R
@@ -6,6 +6,9 @@ library(shinyalert)
 library(markdown)
 library(RMySQL)
 
+# Source separate UI files for each tab
+source('upload_ui.R')
+
 # function that appends elements to a navbar
 # intended purpose is to add a text search input in navbar
 # but can be applied with multiple different elements
@@ -25,82 +28,7 @@ ui <- fluidPage(
     navbarPageWithInputs(
         # original navbar setup
         "SigRepo",
-        # upload signature page structure
-        tabPanel("Upload",
-            sidebarLayout(
-                sidebarPanel(
-                    fileInput(
-                        inputId = "rds_file_1",
-                        label = "Signature File",
-                        multiple = FALSE,
-                        accept = c("text/csv","text/rds",".rds"),
-                        width = NULL,
-                        buttonLabel = "Browse...",
-                        placeholder = "No file selected"
-                    ),
-                    fileInput(
-                      inputId = "rds_file_2",
-                      label = "Signature File",
-                      multiple = FALSE,
-                      accept = c("text/csv","text/rds",".rds"),
-                      width = NULL,
-                      buttonLabel = "Browse...",
-                      placeholder = "No file selected"
-                    ),
-                    fileInput(
-                      inputId = "rds_file_3",
-                      label = "Signature File",
-                      multiple = FALSE,
-                      accept = c("text/csv","text/rds",".rds"),
-                      width = NULL,
-                      buttonLabel = "Browse...",
-                      placeholder = "No file selected"
-                    ),
-                    HTML(
-                        paste(
-                            "Would be good to link to an example rds file here",
-                            "(Maybe for all levels)"
-                        ),
-                        sep = "<br/></br>"
-                    ),
-                    textInput(
-                        inputId = "signature_name",
-                        label = "Signature Name",
-                        placeholder = NULL
-                    ),
-                    textInput(
-                        inputId = "group_id",
-                        label = "Collection Name",
-                        placeholder = NULL
-                    ),
-                    selectizeInput(
-                        inputId = "species_id",
-                        label = "Species",
-                        choices = NULL
-                    ),
-                    selectizeInput(
-                        inputId = "platform_id",
-                        label = "Platform",
-                        choices = NULL
-                    ),
-                    textInput(
-                        inputId = "cell_line",
-                        label = "Cell Line(ATCC)",
-                        placeholder = NULL
-                    ),
-                    selectizeInput(
-                        inputId = "keywords",
-                        label = "Keywords(Optional)",
-                        multiple = TRUE,
-                        choices = c("a", "b", "c"),
-                        options=list(create=TRUE)
-                    ),
-                    actionButton("add_signature", "Add Signature")
-                ),
-                
-                # 
-                mainPanel("could put something here")
-            )),
+        upload_ui,
         # Summary page. has placeholder elements for now
         tabPanel("Summary",
             tableOutput("featurename_table")),
