7a43a7f3f0de533e69d240c7dabcee52be4a3b55
RebekahLMiller <RebekahLMiller@gmail.com>
2020-02-11 16:03:36 -0500

    Added new file with database related functions

--- search/search_server.R
+++ search/search_server.R
@@ -1,25 +1,6 @@
 # Server logic for search page
 
-# Get choices for species
-get_species <- reactive({
-  # Connect to database
-  species_handle <- new_conn_handle()
-  # Disconnect from database when exiting get_species()
-  on.exit(dbDisconnect(species_handle, add = TRUE))
-  # Query database
-  species_obj <- dbGetQuery(
-    species_handle,
-    statement = "
-                        select
-                            concat(species,'[',taxonomic_id,']')
-                            as species
-                        from species;
-                        "
-  )
-  # Return results of query
-  return((species_obj$species))
-})
-# Update options in dropdown menu
+# Update species checkboxes with list of species from database 
 observe({
   updateCheckboxGroupInput(session,
     "search_species",
@@ -27,6 +8,12 @@ observe({
   )
 })
 
+# Show which species have been selected so far
 output$search_selected_species <- renderText(
-  c("<p><font size=3><b>", "Selected Species:</b></font></p><p><font size=2>", input$search_species, "</p></font>")
+    c(
+        "<p><font size=3><b>",
+        "Selected Species:</b></font></p><p><font size=2>",
+        input$search_species,
+        "</p></font>"
+    )
 )
