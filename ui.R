c8c534191a183271a672f47ec28a1e0ed91b8364
RebekahLMiller <RebekahLMiller@gmail.com>
2020-02-11 16:30:05 -0500

    Removed summary page and added comments

--- ui.R
+++ ui.R
@@ -11,34 +11,31 @@ source('upload/upload_ui.R')
 source('search/search_ui.R')
 source('compare/compare_ui.R')
 
-# function that appends elements to a navbar
-# intended purpose is to add a text search input in navbar
-# but can be applied with multiple different elements
+# Function that appends elements to a navbar
+# Intended purpose is to add a text search input in navbar,
+#   but can be applied with multiple different elements
 navbarPageWithInputs <- function(..., inputs) {
     navbar <- navbarPage(...)
     form <- tags$form(class = "navbar-form", inputs)
-    navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(navbar[[3]][[1]]$children[[1]], form)
+    navbar[[3]][[1]]$children[[1]] <-
+        htmltools::tagAppendChild(navbar[[3]][[1]]$children[[1]], form)
     navbar
 }
 
 # Define UI
 ui <- fluidPage(
-    #allows for alert feature
+    # Allows for alert feature
     useShinyalert(),
     theme = shinytheme("sandstone"),
+    # Page header/title
     tags$h1("SigRepo"),
     navbarPageWithInputs(
-        # original navbar setup
+        # Navbar header
         "SigRepo",
-        # upload signature page structure
+        # UI structure for each separate tab
         upload_ui,
-        # search signatures page structure
         search_ui,
-        # compare signature page structure
         compare_ui,
-        # Summary page. has placeholder elements for now
-        tabPanel("Summary",
-            tableOutput("featurename_table")),
         # second parameter in navbar appending function above
         inputs = textInput(
             inputId = "search",
