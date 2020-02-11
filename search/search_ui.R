586187e74afc412dc2f23abe0f294f03a8b138e2
RebekahLMiller <RebekahLMiller@gmail.com>
2020-02-11 17:33:17 -0500

    Removed dropdown menus

--- search/search_ui.R
+++ search/search_ui.R
@@ -2,42 +2,22 @@
 search_ui <- tabPanel("Search",
     sidebarLayout(
         sidebarPanel(
-            textInput(
-                inputId = "search_signature_name",
-                label = "Signature Name",
-                placeholder = NULL
-            ),
-            textInput(
-                inputId = "search_group_id",
-                label = "Collection Name",
-                placeholder = NULL
-            ),
-            htmlOutput(
-                outputId = "search_selected_species"
-            ),
-            checkboxGroupInput("search_species",
+            htmlOutput(outputId = "search_selected_species"),
+            htmlOutput(outputId = "search_selected_platforms"),
+            selectizeInput(
+                "search_species",
                 label = "Choose species",
                 choices = NULL,
-                selected = NULL),
+                multiple = TRUE
+            ),
             selectizeInput(
                 inputId = "search_platform_name",
                 label = "Platform",
-                choices = NULL
-            ),
-            textInput(
-                inputId = "search_cell_line",
-                label = "Cell Line(ATCC)",
-                placeholder = NULL
-            ),
-            selectizeInput(
-                inputId = "search_keywords",
-                label = "Keywords",
-                multiple = TRUE,
-                choices = c("a", "b", "c"),
-                options=list(create=TRUE)
+                choices = NULL,
+                multiple = TRUE
             )
         ),
         
-        # 
+        #
         mainPanel("Display table of matching signatures?")
     ))
