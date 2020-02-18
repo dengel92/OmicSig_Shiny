10487200d55021bccde46779f5ddbb61600cded9
Vanessa Mengze Li <jellyfish@sea.com>
2020-02-18 17:37:33 -0500

    implement function to compare two signatures. output: Venn, unique signatures, common signatures among the two, hypergeometric test p-val, and original signatures for reference.

--- compare/compare_server.R
+++ compare/compare_server.R
@@ -1,39 +1,76 @@
 # Update signature name dropdown with list from database
-#if you guys know a more generic/one shot way of updating all selectize inputs at once, 
-#please let me know. Although, later on, we'll need to update the selectize inputs downstream
-#to not include the name(s) of the other selectize inputs
+# if you guys know a more generic/one shot way of updating all selectize inputs at once,
+# please let me know. Although, later on, we'll need to update the selectize inputs downstream
+# to not include the name(s) of the other selectize inputs
+source("server_functions/Function_sigCompare.R")
+
 observe({
-    updateSelectizeInput(
-        session,
-        "compare_1",
-        choices = c(
-            "",
-            get_signature_names()
-            )
+  updateSelectizeInput(
+    session,
+    "compare_1",
+    choices = c(
+      "",
+      get_signature_names()
     )
-    updateSelectizeInput(
-        session,
-        "compare_2",
-        choices = c(
-            "",
-            get_signature_names()
-        )
+  )
+  updateSelectizeInput(
+    session,
+    "compare_2",
+    choices = c(
+      "",
+      get_signature_names()
     )
+  )
 })
 
-#results of compare signatures.
-#this part is where we need to call the compare function
-#and likely change the output nature to "plot" instead of "text" here
-#Tuturu~
-observeEvent(
-    input$compare_signatures,{
-        #you'll want check functions here to make sure there aren't any empty signature name entries
-        output$compare_result <- renderText(
-            sql_generic(
-                paste(
-                    "select feature_name from feature_signature_view where signature_name =",
-                    single_quoted(input$compare_1),";",
-                    sep=""
-                )
-            )$feature_name
-    )})
+# results of compare signatures:
+# Tuturu~
+# Meow!
+compare_result_variable <- eventReactive(input$compare_signatures, {
+  sig1 <- sql_generic(
+    paste(
+      "select feature_name from feature_signature_view where signature_name =",
+      single_quoted(input$compare_1), ";",
+      sep = ""
+    )
+  )$feature_name
+  sig2 <- sql_generic(
+    paste(
+      "select feature_name from feature_signature_view where signature_name =",
+      single_quoted(input$compare_2), ";",
+      sep = ""
+    )
+  )$feature_name
+  return(sigCompare_two(sig1, sig2, is.lv2 = FALSE, background_number = input$compare_background_number))
+  # names: c("Venn", "only_sig1", "only_sig2", "sig_both", "hyper_p.value")
+})
+output$compare_result_Venn <- renderPlot(compare_result_variable()$Venn)
+
+output$compare_result <- renderText({
+  c(
+    "<font face = \"PT Sans\",font size=3>",
+    "<p>The signatures you are comparing:",
+    "<br><font color=\"#228822\">", input$compare_1, "(", length(compare_result_variable()$sig1), " signatures found )", " </font> ",
+    "and <font color=\"#881199\">", input$compare_2, "(", length(compare_result_variable()$sig2), " signatures found )", "</font></p>",
+    "<p>",
+    "<p><i>The unique signatures in ", input$compare_1, " are:</i>",
+    "<br>", compare_result_variable()$only_sig1,
+    "<br><i>The unique signatures in ", input$compare_2, " are:</i>",
+    "<br>", compare_result_variable()$only_sig2,
+    "<br><i>The signatures appeared in both lists are:</i>",
+    "<br>", compare_result_variable()$sig_both, "</p>",
+    "<p> Please see the Venn diagram below for a better visualization! </p>",
+    "<p>After the Hyper Geometric test, with the background set as ", input$compare_background_number, " genes / proteins: ",
+    "<br>the p-value we got is <b>", compare_result_variable()$hyper_p.value, "</b>.</p>",
+    "<p><font color=\"#AB6611\"><b><i> Thank you for using! </font></b></i></p>",
+    "</font>"
+  )
+})
+# show_signatures_variable <- eventReactive(input$compare_show_signatures,{compare_result_variable()$})
+
+output$compare_show_signatures <- renderText({
+  c(
+    "<p><i>All signatures in", input$compare_1, ":</i> <br><font color=\"#228822\">", compare_result_variable()$sig1, "</font></p>",
+    "<p><i>All signatures in", input$compare_2, ":</i> <br><font color=\"#881199\">", compare_result_variable()$sig2, "</font></p>"
+  )
+})
--- compare/compare_ui.R
+++ compare/compare_ui.R
@@ -1,24 +1,28 @@
 compare_ui <- tabPanel(
-    "Compare",
-    #If you guys know a cooler/different layout from sidebarLayout,
-    #Please tell me, please
-    sidebarLayout(
-        sidebarPanel(
-            selectizeInput(
-                "compare_1",
-                label = "Signature 1",
-                choices = NULL,
-                multiple = FALSE
-            ),            
-            selectizeInput(
-                "compare_2",
-                label = "Signature 2",
-                choices = NULL,
-                multiple = FALSE
-            ),
-        actionButton("compare_signatures", "Compare Signatures")),
-        mainPanel(
-            textOutput("compare_result")    
-        )
+  "Compare",
+  # If you guys know a cooler/different layout from sidebarLayout,
+  # Please tell me, please
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput(
+        "compare_1",
+        label = "Signature 1",
+        choices = NULL,
+        multiple = FALSE
+      ),
+      selectizeInput(
+        "compare_2",
+        label = "Signature 2",
+        choices = NULL,
+        multiple = FALSE
+      ),
+      numericInput("compare_background_number", label = "Background", value = 22000),
+      actionButton("compare_signatures", "Compare Signatures")
+    ),
+    mainPanel(
+      htmlOutput("compare_result"),
+      plotOutput("compare_result_Venn"),
+      htmlOutput("compare_show_signatures")
     )
+  )
 )
