1903e240380eb335f5b31681382fc288712a40e9
RebekahLMiller <RebekahLMiller@gmail.com>
2020-02-20 18:30:15 -0500

    Fixed dropdown menu updating issues

--- search/search_server.R
+++ search/search_server.R
@@ -1,48 +1,63 @@
 # Server logic for search page
 
-# Update species dropdown with list of species from database
-observe({
-    updateSelectizeInput(session,
-        "search_species",
-        choices = c("", get_species()))
-})
-
-# Show which species have been selected so far
-output$search_selected_species <- renderText(
+# Show which search terms have been selected so far
+output$search_terms <- renderText(
     c(
         "<p><font size=3><b>",
-        "Selected Species:</b></font></p><p><font size=2>",
+        "Selected Search Terms:</b></font></p><p><font size=2>",
+        # Show selected species
         if (length(input$search_species) < 1) {
-            "No species selected"
+            "No species selected</br>"
         } else {
-            input$search_species},
+            paste(paste(input$search_species, collapse = ', '), '</br>')
+        },
+        # Show selected platforms
+        if (length(input$search_platform_name) < 1) {
+            "No platforms selected"
+        } else {
+            paste(input$search_platform_name, collapse = ', ')
+        },
         "</p></font>"
     )
 )
 
+# Update species dropdown with list of species from database
+observe({
+    updateSelectizeInput(session,
+        "search_species",
+        choices = get_species())
+})
+
 # Update other dropdowns based on input to species
-observeEvent(input$search_species, {
-    # If a species is selected
-    if (!is.null(input$search_species)) {
-        # Query database to find platforms matching selected species
-        sql_obj <-
-          sql_finding_query(
-                fields=c("platform_name"),
-                target_table = "platform_signature_view",
-                wheres = list("species" = input$search_species)
+observeEvent(input$search_species,
+    ignoreNULL = FALSE,
+    handlerExpr = {
+        # If a species is selected
+        if (!is.null(input$search_species)) {
+            # Query database to find platforms matching selected species
+            sql_obj <-
+                sql_finding_query(
+                    fields = "platform_name",
+                    target_table = "platform_signature_view",
+                    wheres = list("species" = input$search_species)
                 )
-        # Update platform dropdown
-        updateSelectizeInput(session,
-            "search_platform_name",
-            choices = bind_rows(sql_obj)$platform_name)
-    } else {
-        # If user unselects all species, use full list of platforms again
-        updateSelectizeInput(session,
-            "search_platform_name",
-            choices = get_platforms())
-    }
-},
-    ignoreNULL = FALSE)
+            # Update platform dropdown
+            updateSelectizeInput(
+                session,
+                "search_platform_name",
+                choices = c(sql_obj$platform_name, input$search_platform_name),
+                selected = input$search_platform_name
+            )
+        } else {
+            # If user unselects all species, use full list of platforms again
+            updateSelectizeInput(
+                session,
+                "search_platform_name",
+                choices = get_platforms(),
+                selected = input$search_platform_name
+            )
+        }
+    })
 
 # Update platform dropdown with list of platforms from database
 observe({
@@ -51,41 +66,58 @@ observe({
         choices = get_platforms())
 })
 
-# Show which platforms have been selected so far
-output$search_selected_platforms <- renderText(
-    c(
-        "<p><font size=3><b>",
-        "Selected Platforms:</b></font></p><p><font size=2>",
-        if (length(input$search_platform_name) < 1) {
-            "No platforms selected"
+# Update other dropdowns based on input to platform
+observeEvent(input$search_platform_name,
+    ignoreNULL = FALSE,
+    handlerExpr = {
+        # If a platform is selected
+        if (!is.null(input$search_platform_name)) {
+            # Query database to find species matching selected platform(s)
+            sql_obj <-
+                sql_finding_query(
+                    fields = "species",
+                    target_table = "platform_signature_view",
+                    wheres = list("platform_name" = input$search_platform_name)
+                )
+            # Update species dropdown
+            updateSelectizeInput(
+                session,
+                "search_species",
+                choices = c(sql_obj$species, input$search_species),
+                selected = input$search_species
+            )
         } else {
-            input$search_platform_name},
-        "</p></font>"
-    )
-)
+            # If user unselects all platforms, use full list of species again
+            updateSelectizeInput(
+                session,
+                "search_species",
+                choices = get_species(),
+                selected = input$search_species
+            )
+        }
+    })
 
-
-### NOT FULLY IMPLEMENTED YET
-### Currently works only if you select something from each dropdown
 # Show table of matching signatures when you hit the search button
 observeEvent(input$search, {
+    # Construct where clause for species
     where_species = NULL
     if (!is.null(input$search_species)) {
         where_species = list('species' = input$search_species)
     }
+    # Construct where clause for platforms
     where_platform = NULL
     if (!is.null(input$search_platform_name)) {
         where_platform = list('platform_name' = input$search_platform_name)
     }
+    # Combine all where clauses
+    wheres = c(where_species, where_platform)
     output$search_results <- renderTable({
         # Ensure that the table updates only once, immediately after clicking
         isolate(
             # Search database for matching signatures
             sql_obj <-
-                sql_finding_query(
-                    target_table = "platform_signature_view",
-                    wheres = c(where_species, where_platform)
-                )
+                sql_finding_query(target_table = "platform_signature_view",
+                    wheres = wheres)
         )
         return(sql_obj)
     })
