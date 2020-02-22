1c0a32a778a0e2ff9f929169c25b87418147eb70
RebekahLMiller <RebekahLMiller@gmail.com>
2020-02-21 23:29:04 -0500

    Added scrollbar to main panel output table

--- search/search_ui.R
+++ search/search_ui.R
@@ -1,24 +1,36 @@
-# search signatures page structure
+# Search signatures page structure
 search_ui <- tabPanel("Search",
+    # Create a layout with a sidebar and a main panel
     sidebarLayout(
+        # Add the sidebar
         sidebarPanel(
+            # Show selected search terms
             htmlOutput(outputId = "search_terms"),
+            
+            # Dropdown menu for selecting species
             selectizeInput(
                 "search_species",
                 label = "Choose species",
                 choices = NULL,
                 multiple = TRUE
             ),
+            
+            # Dropdown menu for selecting platform
             selectizeInput(
                 inputId = "search_platform_name",
                 label = "Choose platform",
                 choices = NULL,
                 multiple = TRUE
             ),
+            
+            # Button to submit search terms
             actionButton("search", "Search Signatures")
         ),
         
+        # Add the main panel
         mainPanel(
-            tableOutput("search_results")
-        )
+            # If the output table is too wide, add a scrollbar
+            style = "overflow-y:scroll",
+            # Show table of signatures matching search terms
+            tableOutput("search_results"))
     ))
