586a07c5122a5da5a73c0fbac0d624f1625cc080
Vanessa Mengze Li <jellyfish@sea.com>
2020-02-24 17:13:43 -0500

    update check_signatures() since signature is saved as dataframe instead of list in OmicSig obj

--- Omic.obj/Function_objCheck.R
+++ Omic.obj/Function_objCheck.R
@@ -11,13 +11,13 @@ check_metadata <- function(metadata) {
     return(paste("Error: Metadata not found in OmicSignature object, or metadata is not a list. "))
   }
 
-  metadata_required <- c("organism", "platform", "type")
+  metadata_required <- c("organism", "platform", "type", "phenotype")
   metadata_missing <- setdiff(metadata_required, names(metadata))
   print(paste("Input metadata have to contain columns: ", paste(metadata_required, collapse = ", "), " .", sep = ""))
 
   if (length(metadata_missing) == 0) {
     print(paste("Checked. Metadata contains all the essential attributes."))
-    return(TRUE)
+    return("finished.")
   } else {
     return(paste("Warning: Metadata does not contain attribute(s): ",
       paste(metadata_missing, collapse = ", "),
@@ -30,71 +30,79 @@ check_metadata <- function(metadata) {
 #----------------------------------------------
 # we need the information of "signature_type" in the metadata
 # so, the input need to either be OmicSig Obj, or need to specify signature type
-check_signatures <- function(omic.obj, signature_type = NULL) {
-
-  # read the signature, and check if it is a list:
+check_signatures <- function(omic.obj, signature_type = NULL, sample_number = 0) {
+  # read the signature, and check if it is a dataframe:
   if (class(omic.obj)[1] == "OmicSignature") {
     signatures <- omic.obj$signatures
     signature_type <- omic.obj$metadata$type
+    if (signature_type == "multi-directional") {
+      sample_number <- omic.obj$metadata$sample_number
+    }
   }
-  else if (class(omic.obj)[1] == "list") {
+  else if (class(omic.obj)[1] == "data.frame") {
     signatures <- omic.obj
     remove(omic.obj)
+  } else {
+    return(paste("Error: Signature not found in OmicSignature object, or signature is not a dataframe. ", sep = ""))
   }
-  else {
-    return(paste("Error: Signature not found in OmicSignature object, or signature is not a list. ", sep = ""))
-  }
-
-  # signatures should be of proper length, according to the "type":
-  if (signature_type == "multi-directional") {
-    sample_number <- omic.obj$metadata$sample_number
+  if (nrow(signatures) == 0) {
+    return(paste("Error: Signature is empty."))
   }
 
+  # check if signature_symbol and signature_score (lv2 or lv3) exists:
+  if (!c("signature_symbol") %in% colnames(signatures)) {
+    return(paste("Error: signature dataframe does not contain \"signature_symbol\" column."))
+  }
+  if (c("signature_score") %in% colnames(signatures)) {
+    print(paste("This signature contains lv2 data."))
+  } else {
+    print(paste("This signature only contains lv3 data."))
+  }
+
+  # check if the direction match with signature type:
+  if (is.null(signature_type)) {
+    return(paste("Error: signature type not specified."))
+  }
+  # each signature type need to be "else if". because if none of the type meet the criteria required, we need an "else" to output error.
   # bi-directional signature:
-  # note: each signature type need to be in "else if",
-  # because if none of the type meet the criteria required, we need an "else" to output error.
-  if (signature_type == "bi-directional" && length(signatures) == 4) {
-    print(paste("Checked. signature is bi-directional with length of 4. "))
-    if ("Up_Regulated_Symbol" %in% names(signatures) &&
-      "Dn_Regulated_Symbol" %in% names(signatures) &&
-      "Up_Regulated_Score" %in% names(signatures) &&
-      "Dn_Regulated_Symbol" %in% names(signatures)) {
-      print(paste("Checked. signature contains *Up Regulated* and *Dn Regulated* elements. "))
-      signatures$Up_Regulated_Symbol <- as.character(signatures$Up_Regulated_Symbol)
-      signatures$Dn_Regulated_Symbol <- as.character(signatures$Dn_Regulated_Symbol)
-      signatures$Up_Regulated_Score <- as.numeric(signatures$Up_Regulated_Score)
-      signatures$Dn_Regulated_Score <- as.numeric(signatures$Dn_Regulated_Score)
-      if ((length(signatures$Up_Regulated_Symbol) + length(signatures$Dn_Regulated_Symbol)) == 0) {
-        return(paste("Warning: No signatures saved in the Omic Object."))
-      } else if ((length(signatures$Up_Regulated_Symbol) * length(signatures$Dn_Regulated_Symbol)) == 0) {
-        print(paste("Warning: Only one of the bi-directional signature is found."))
-      }
+  if (signature_type == "bi-directional") {
+    summary_direction <- summary(signatures$signature_direction)
+    if (isTRUE(all.equal(c("Dn", "Up"), names(summary(signatures$signature_direction))))) {
+      print("Checked. signature is bi-directional with Dn and Up directions.")
+    } else if (names(summary_direction) == c("Dn") | names(summary_direction) == c("Up")) {
+      print("Checked. signature is bi-directional, but only one direction is found.")
     } else {
-      print(paste("Warning: Signature need to be named as Up_Regulated_Symbol, Dn_Regulated_Symbol, Up_Regulated_Score, Dn_Regulated_Score. This can cause problem when you trying to save the OmicSig object."))
+      return(paste("Error: bi-directional signature should be marked with \"Dn\" and \"Up\"."))
     }
   }
 
   # uni-directional signature:
-  else if (signature_type == "uni-directional" && length(signatures) == 2) {
-    print(paste("Checked. Signature is uni-directional with length of 2. "))
-    if ("Signature_Symbol" %in% names(signatures) && "Signature_Score" %in% names(signatures)) {
-      print(paste("Checked. Signature contains *Signature_Symbol* and *Signature_Score* elements. "))
+  else if (signature_type == "uni-directional") {
+    if (!"signature_direction" %in% colnames(signatures)) {
+      print(paste("Checked. signature is uni-directional. "))
     } else {
-      print(paste("Warning: Signature need to be named as Signature_Symbol, Signature_Score. This can cause problem when you trying to save the OmicSig object."))
+      print(paste("Warning: signature is specified as uni-directional but additional direction information found."))
     }
   }
 
   # multi-directional signature:
-  else if (signature_type == "multi-directional" && length(signatures) == sample_number) {
-    print(paste("Checked. Signature is multi-directional with", sample_number, "samples.", sep = " "))
+  else if (signature_type == "multi-directional") {
+    summary_direction <- summary(signatures$signature_direction)
+    if (length(summary_direction) == sample_number) {
+      print(paste("Checked. signature is multi-directional with", sample_number, "samples.", sep = " "))
+    } else if (length(summary_direction) < sample_number) {
+      print(paste("Warning: signature is multi-directional with", sample_number, "samples, but some samples do not have signatures."))
+    } else {
+      return(paste("Error: signature is specified as multi-directional, but sample number is invalid."))
+    }
   }
 
   # if none of the signature type is meet:
   else {
-    return(paste("Error: Signature is not a list with proper length. Or the sample_number is not specified in a multi-directional signature."))
+    return(paste("Error: Signature information invalid."))
   }
 
-  return(TRUE)
+  return("finished.")
   # note: except erroes, all the output in check_signature are "print" now
   #       have not "locked" the return yet in case in the future we need to check more things
 }
@@ -155,7 +163,7 @@ check_difexp <- function(omic.obj) {
     }
   }
 
-  return(TRUE)
+  return("finished.")
   # note: except errors, all the output in check_difexp are "print" now
   #       have not "locked" the return yet in case in the future we need to check more things
 }
