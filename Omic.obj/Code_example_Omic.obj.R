c81586780fcb5679ed86fe6d4463020cf657dd55
Vanessa Mengze Li <jellyfish@sea.com>
2020-02-24 18:17:25 -0500

    minor update: use a better example file

--- Omic.obj/Code_example_Omic.obj.R
+++ Omic.obj/Code_example_Omic.obj.R
@@ -4,7 +4,7 @@ source("Omic.obj/Function_json.R")
 source("Omic.obj/Function_objCheck.R")
 source("Omic.obj/OmicObj.R")
 
-Omic.obj.new <- read_json("Omic.obj/signatures/MDA_AhR_obj.txt")
+Omic.obj.new <- read_json("Omic.obj/signatures/Cal27_CB113_obj.txt")
 
 # check functions:
 check_metadata(Omic.obj.new)
@@ -13,7 +13,7 @@ check_difexp(Omic.obj.new)
 # view signatures:
 Omic.obj.new$signatures
 # use new criteria to extract signatures:
-Omic.obj.new$extract.signature("logFC < -1; fdr < 0.001")
+Omic.obj.new$extract.signature("logFC < -1; fdr < 0.05")
 # write files:
 write_obj(Omic.obj.new,"Omic.obj/example_Omic.obj.txt")
 write.table(Omic.obj.new$difexp, "Omic.obj/example_lv1.txt", sep = "\t", quote = F, col.names = T, row.names = F)
