c29065b1f75c92555ea844d63effb8acf57d6da8
RebekahLMiller <RebekahLMiller@gmail.com>
2020-03-25 08:09:09 -0400

    Tried to fix accordion menus once again without success

--- SigRepoInterface/help/help_ui.R
+++ SigRepoInterface/help/help_ui.R
@@ -1,53 +1,3 @@
-# # Help page structure
-# 
-# upload_titles <-
-#     c("Here is a frequently asked question",
-#         "And another one",
-#         "And a third")
-# upload_contents <- list(
-#     "This is the answer to the first faq",
-#     "This is the answer to the second faq",
-#     "This is the answer to the third faq"
-# )
-# 
-# search_titles <-
-#     c("Here is a frequently asked question",
-#         "And another one",
-#         "And a third")
-# search_contents <- list(
-#     "This is the answer to the first faq",
-#     "This is the answer to the second faq",
-#     "This is the answer to the third faq"
-# )
-# 
-# help_ui <- tabPanel("Help",
-#     sidebarLayout(sidebarPanel(
-#         tabPanel(
-#             "Upload help",
-#             htmlOutput(outputId = "upload_help"),
-#             dq_space(),
-#             dq_accordion(
-#                 "upload_faqs",
-#                 upload_titles,
-#                 upload_contents,
-#                 options = list(active = FALSE, collapsible = TRUE),
-#                 bg_color = "#f8f5f0"
-#             )
-#         ),
-#         tabPanel(
-#             "Search help",
-#             htmlOutput(outputId = "search_help"),
-#             dq_space(),
-#             dq_accordion(
-#                 "search_faqs",
-#                 search_titles,
-#                 search_contents,
-#                 options = list(active = FALSE, collapsible = TRUE),
-#                 bg_color = "#f8f5f0"
-#             )
-#         )
-#     )))
-
 # Help page structure
 
 upload_titles <-
@@ -59,6 +9,15 @@ upload_contents <- list(
     "This is the answer to the second faq",
     "This is the answer to the third faq"
 )
+upload_help_tab <- tabPanel(
+    "Upload help",
+    htmlOutput(outputId = "upload_help"),
+    dq_accordion("upload_faqs",
+        upload_titles,
+        upload_contents,
+        options = list(active = FALSE, collapsible = TRUE),
+        bg_color = "#f8f5f0")
+)
 
 search_titles <-
     c("Here is a frequently asked question",
@@ -69,26 +28,19 @@ search_contents <- list(
     "This is the answer to the second faq",
     "This is the answer to the third faq"
 )
+search_help_tab <- tabPanel(
+    "Search help",
+    htmlOutput(outputId = "search_help"),
+    dq_accordion("search_faqs",
+        search_titles,
+        search_contents,
+        options = list(active = FALSE, collapsible = TRUE),
+        bg_color = "#f8f5f0")
+)
 
 help_ui <- tabPanel("Help",
     navbarPage(
         "",
-        tabPanel(
-            "Upload help",
-            htmlOutput(outputId = "upload_help"),
-            dq_accordion("upload_faqs",
-                upload_titles,
-                upload_contents,
-                options = list(active = FALSE, collapsible = TRUE),
-                bg_color = "#f8f5f0")
-        ),
-        tabPanel(
-            "Search help",
-            htmlOutput(outputId = "search_help"),
-            dq_accordion("search_faqs",
-                search_titles,
-                search_contents,
-                options = list(active = FALSE, collapsible = TRUE),
-                bg_color = "#f8f5f0")
-        )
+        upload_help_tab,
+        search_help_tab
     ))
