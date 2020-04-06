e52806ff1fb659d0d97008b1c1526212aee29914
cbragdon93 <cjoseph@bu.edu>
2020-04-06 17:48:28 -0400

    leftover from previous commit. forgot to stage

--- SigRepoInterface/server.R
+++ SigRepoInterface/server.R
@@ -10,15 +10,21 @@ library(stringr)
 server <- shinyServer(function(input, output, session) {
     # Source database related functions
     # local = TRUE -> objects are inside server function
-    #source('../command_line/database_functions.R', local = TRUE)
-    
+    source('../command_line/database_functions.R', local = TRUE)
+    source('server_functions/database_functions_interface.R')
+    # Source OmicSignature Function Files 
+    source('../OmicSignature/check_functions/Function_json.R')
+    source('../OmicSignature/check_functions/Function_objCheck.R')
+    source('../OmicSignature/check_functions/Function_write_sig.R')
+    source('../OmicSignature/OmicSignature.R')
+    #
     # Source separate server files for each tab
     # local = TRUE -> objects are inside server function
     source('help/help_server.R', local = TRUE)
     source('compare/compare_server.R' , local = TRUE)
     source('compare_bulk/compare_bulk_server.R' , local = TRUE)
     source('server_functions/comparing/compare_heatmap.R' , local = TRUE)   
-    #source('upload/upload_server.R', local = TRUE)
+    source('upload/upload_server.R', local = TRUE)
     source('search/search_server.R', local = TRUE)
     source('hypeR_overrep/hypeR_overrep_server.R' , local = TRUE)
 })
