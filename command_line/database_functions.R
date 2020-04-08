ef26fdb4a9ffde2ea352d2bdafac4c8b07310564
RebekahLMiller <RebekahLMiller@gmail.com>
2020-04-08 17:02:41 -0400

    Added between clauses to sql_finding_query

--- command_line/database_functions.R
+++ command_line/database_functions.R
@@ -7,14 +7,14 @@ library(plyr)
 # Database related functions
 
 #' Add single quotation marks around a string
-#' 
+#'
 #' @param my_string the string to add single quotation marks to
 single_quoted <- function(my_string) {
     return(paste0("'", my_string, "'"))
 }
 
 #' Create a link to a signature file based on the signature name
-#' 
+#'
 #' @param signature_name the name of the signature to link to
 create_link <-
     function(signature_name) {
@@ -43,7 +43,7 @@ new_conn_handle <- function() {
 }
 
 #' Submit a query to the database
-#' 
+#'
 #' @param query the query to submit
 sql_generic <- function(query) {
     # Connect to the database
@@ -60,7 +60,8 @@ sql_generic <- function(query) {
 #' @param mylist named list where the names are fields and the values are values
 #'   associated with those fields
 #' @param list_key the field name to access from mylist
-in_paste <- function(mylist, list_key) {
+construct_in_clause <- function(mylist, list_key) {
+    # Construct clause
     in_clause <- paste0(list_key,
         " IN (",
         paste(single_quoted(mylist[[list_key]]), collapse = ","),
@@ -73,17 +74,52 @@ in_paste <- function(mylist, list_key) {
     return(in_clause)
 }
 
+#' Assemble part of a where clause of the form
+#'   "<field> BETWEEN <value1> AND <value2>"
+#'
+#' @param mylist named list where the names are fields and the values are values
+#'   associated with those fields
+#' @param list_key the field name to access from mylist
+#'
+#' Note that mylist[[list_key]] must have exactly two values
+construct_between_clause <- function(mylist, list_key) {
+    # Make sure mylist[[list_key]] has exactly two values
+    if (length(mylist[[list_key]]) != 2) {
+        stop(paste0(
+            "You tried to construct a between clause with ",
+            length(mylist[[list_key]]),
+            " element(s)!"
+        ))
+    }
+    # Construct clause
+    between_clause <- paste0(list_key,
+        " BETWEEN ",
+        paste0(single_quoted(mylist[[list_key]][1:2]), collapse = " AND "))
+    return(between_clause)
+}
+
 #' Constructs sql query based on where clause, if one is needed,
 #'   and executes final query as output
 #'
 #' @param fields character vector, the fields to select from the target table
 #' @param target_table string; the table to select from
-#' @param wheres named list where the names are the fields to narrow search by
+#' @param ins named list where the names are the fields to narrow search by
 #'   and the values are vectors of the values to look for in those fields
+#' @param betweens named list where the names are the fields to narrow search by
+#'   and the values are vectors of length 2 containing the endpoints of the
+#'   range to consider in those fields
+#'
+#' @example
+#' 
+#' sql_finding_query("platform_signature_view", fields = c("*"),
+#'     ins = list("species" = c("Homo sapiens"),
+#'         "signature_name" = c("Cal27_BaP", "Cal27_PYO")),
+#'     betweens = list("upload_date" = c("2020-01-01", "2020-03-01")))
 sql_finding_query <-
     function(target_table,
         fields = c("*"),
-        wheres = NULL) {
+        ins = NULL,
+        betweens = NULL) {
         # Query construction
         sql <- paste("SELECT ",
             paste(fields, collapse = ","),
@@ -102,20 +138,32 @@ sql_finding_query <-
         # values resulted in zero records from the DB.
         where_clauses = ''
         # Removes elements where the value is null
-        wheres <- compact(wheres)
-        if (!is.null(wheres) && length(wheres) > 0) {
-            # Assemble the part of each where clause of the form
+        ins <- compact(ins)
+        betweens <- compact(betweens)
+        if (!is.null(ins) && length(ins) > 0) {
+            # Assemble the part of each in clause of the form
             #   "<field> IN (<field_values>)"
-            ins <- lapply(names(wheres), in_paste, mylist = wheres)
-            # Add "WHERE" to the beginning of the where clauses and separate
-            #   each "<field> IN (<field_values>)" clause by " AND "
+            ins <-
+                lapply(names(ins), construct_in_clause, mylist = ins)
+        }
+        if (!is.null(betweens) && length(betweens) > 0) {
+            # Assemble the part of each between clause of the form
+            #   "<field> BETWEEN <value1> AND <value2>"
+            betweens <-
+                lapply(names(betweens), construct_between_clause,
+                    mylist = betweens)
+        }
+        if (!is.null(c(ins, betweens)) &&
+                length(c(ins, betweens)) > 0) {
+            # Add "WHERE" to the beginning of the clauses and separate
+            #   each clause by " AND "
             where_clauses <-
-                paste("WHERE", paste(ins, collapse = " AND "))
+                paste("WHERE", paste(c(ins, betweens), collapse = " AND "))
         }
         # Add where clauses to query
         sql <- paste(sql, where_clauses, ";", sep = " ")
         #Debugging block
-        if (FALSE) {
+        if (TRUE) {
             print(sql)
         }
         # Execute
