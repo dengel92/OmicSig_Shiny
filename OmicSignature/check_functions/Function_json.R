8d942239a73317343bd718374a624ad1c8e6c70d
Vanessa Mengze Li <jellyfish@sea.com>
2020-05-03 16:13:45 -0400

    change function, variables to camelCase

--- OmicSignature/check_functions/Function_json.R
+++ OmicSignature/check_functions/Function_json.R
@@ -1,57 +1,57 @@
 #######################################################################
-## write_json()
+## writeJson()
 ##
 #' @title write OmicSignature object into json txt format
 #' updated 01/2020
 #'
-#' @param omic.obj A OmicSignature object
+#' @param OmicObj A OmicSignature object
 #' @param file export file name
 #' @return a "finished" message
 #'
 #' @example
-#' write_json(Omic.obj, file = "Omic.json")
+#' writeJson(OmicObj, file = "Omic.json")
 #'
 
-write_json <- function(omic.obj, file) {
-  signature_direction <- summary(omic.obj$signatures$signature_direction)
-  # drop the signature_direction column:
-  write_signature <- omic.obj$signatures
-  write_signature$signature_direction <- NULL
-  metadata_length <- length(omic.obj$metadata)
-  write_json_obj <- jsonlite::toJSON(c(
-    omic.obj$metadata,
-    "metadata_length" = metadata_length,
-    list("signature_direction_names" = names(signature_direction)),
-    signature_direction,
-    write_signature,
-    list("lv1_colnames" = colnames(omic.obj$difexp)),
-    omic.obj$difexp
+writeJson <- function(OmicObj, file) {
+  signatureDirection <- summary(OmicObj$signatures$signatureDirection)
+  # drop the signatureDirection column:
+  writeSignature <- OmicObj$signatures
+  writeSignature$signatureDirection <- NULL
+  metadataLength <- length(OmicObj$metadata)
+  writeJson_obj <- jsonlite::toJSON(c(
+    OmicObj$metadata,
+    "metadataLength" = metadataLength,
+    list("signatureDirectionNames" = names(signatureDirection)),
+    signatureDirection,
+    writeSignature,
+    list("lv1Colnames" = colnames(OmicObj$difexp)),
+    OmicObj$difexp
   ), na = NULL, pretty = T)
-  write(write_json_obj, file)
+  write(writeJson_obj, file)
   return("finished")
 }
 
 #######################################################################
-## read_json()
+## readJson()
 ##
-#' @title read an OmicSignature object from json txt file created by write_json()
+#' @title read an OmicSignature object from json txt file created by writeJson()
 #' updated 01/2020
 #'
 #' @param filename json file name to read in
 #' @return OmicSignature object
 #'
 #' @example
-#' read_json(filename = "Omic.json")
+#' readJson(filename = "Omic.json")
 #'
-read_json <- function(filename) {
-  read_json <- jsonlite::fromJSON(txt = filename)
-  read_meta <- read_json[c(1:read_json$metadata_length)]
-  lv1_colnames <- read_json$lv1_colnames
-  read_lv1 <- data.frame(dplyr::bind_rows(read_json[c(lv1_colnames)]))
-  read_lv2 <- data.frame(dplyr::bind_rows(read_json[c("signature_symbol", "signature_score")]))
-  signature_direction <-
-    rep(read_json$signature_direction_names, unlist(read_json[read_json$signature_direction_names]))
-  read_lv2 <- cbind(read_lv2, signature_direction)
-  read_sig.obj <- OmicSignature$new(read_meta, read_lv2, read_lv1)
-  return(read_sig.obj)
+readJson <- function(filename) {
+  readJson <- jsonlite::fromJSON(txt = filename)
+  readMetadata <- readJson[c(1:readJson$metadataLength)]
+  lv1Colnames <- readJson$lv1Colnames
+  readLv1 <- data.frame(dplyr::bind_rows(readJson[c(lv1Colnames)]))
+  readLv2 <- data.frame(dplyr::bind_rows(readJson[c("signature_symbol", "signature_score")]))
+  signatureDirection <-
+    rep(readJson$signatureDirectionNames, unlist(readJson[readJson$signatureDirectionNames]))
+  readLv2 <- cbind(readLv2, signatureDirection)
+  readSigObj <- OmicSignature$new(readMetadata, readLv2, readLv1)
+  return(readSigObj)
 }
